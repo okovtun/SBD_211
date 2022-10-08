dacd83f31c75848bf1df2b2ced80ed5b29a12dd0
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-08 17:48:54 +0300

    DataTypes day 1

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,14 @@
+//DataTypes
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	//cout << true << endl;
+	//cout << false << endl;
+
+	cout << sizeof(long long) << endl;
+	cout << 0 << " ... " << ULLONG_MAX << endl;
+	cout << LLONG_MIN << " ... " << LLONG_MAX << endl;
+}
